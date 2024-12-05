b844a3cbe18dc966d4da09928fac6c71486ae001
tunashred <enachealexandru.traian@gmail.com>
2024-12-05 09:22:11 +0200

    Create utils dir and move read_file there

--- 2024/day_1/part_2.cpp
+++ 2024/day_1/part_2.cpp
@@ -1,32 +1,11 @@
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <vector>
 #include <algorithm>
 #include <string_view>
 #include <cmath>
 
-// TODO: move this to a utils dir inside 2024 dir
-std::stringstream read_file(std::string_view file_name) {
-    std::ifstream file{file_name.data(), std::ios::in};
-    if (!file) {
-        throw std::runtime_error("Error opening file!");
-    }
-
-    file.seekg(0, std::ios::end);
-    std::streamsize file_size = file.tellg();
-    std::string temp_buffer(file_size, '\0');
-
-    file.seekg(0, std::ios::beg);
-    if (!file.read(temp_buffer.data(), file_size)) {
-        throw std::runtime_error("Error reading file!");
-    }
-
-    std::stringstream stream(std::move(temp_buffer));
-
-    file.close();
-    return stream;
-}
+#include "utils.hpp"
 
 int main() {
     std::stringstream input{read_file("../../day_1/in.input")};
---
+++ 2024/utils/utils.cpp
@@ -0,0 +1,25 @@
+#include <sstream>
+#include <fstream>
+
+#include "utils.hpp"
+
+std::stringstream read_file(std::string_view file_name) {
+    std::ifstream file{file_name.data(), std::ios::in};
+    if (!file) {
+        throw std::runtime_error("Error opening file!");
+    }
+
+    file.seekg(0, std::ios::end);
+    std::streamsize file_size = file.tellg();
+    std::string temp_buffer(file_size, '\0');
+
+    file.seekg(0, std::ios::beg);
+    if (!file.read(temp_buffer.data(), file_size)) {
+        throw std::runtime_error("Error reading file!");
+    }
+
+    std::stringstream stream(std::move(temp_buffer));
+
+    file.close();
+    return stream;
+}
