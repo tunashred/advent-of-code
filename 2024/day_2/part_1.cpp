86995b363ae258c2133223cb0907a109b8aed21f
tunashred <enachealexandru.traian@gmail.com>
2024-12-07 18:41:17 +0200

    Finished part 1

---
+++ 2024/day_2/part_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "utils.hpp"
+
+int main() {
+    std::stringstream stream{read_file("../../day_2/in.input")};
+    std::string line;
+    int result = 0;
+
+    while (std::getline(stream, line)) {
+        std::stringstream line_stream(line);
+        int current, next;
+        bool is_valid = true;
+        bool increasing_order = true;
+
+        line_stream >> current >> next;
+        if (abs(current - next) > 3 || current == next) {
+            continue;
+        }
+        if (current > next) {
+            increasing_order = false;
+        }
+        current = next;
+        
+        while (line_stream >> next) {
+            if (abs(current - next) > 3 || current == next) {
+                is_valid = false;
+                break;
+            }
+            if ((current < next && !increasing_order) || (current > next && increasing_order)) {
+                is_valid = false;
+                break;
+            }
+            current = next;
+        }
+
+        if (is_valid) {
+            std::cout << line_stream.str() << "\n";
+            result++;
+        }
+    }
+
+    std::cout << result << '\n';
+
+    return 0;
+}
