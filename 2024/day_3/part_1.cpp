a2cc002d5dad9ac14ed055c3bfa0c31417104fed
tunashred <enachealexandru.traian@gmail.com>
2025-01-01 18:00:53 +0200

    Finished part 1

---
+++ 2024/day_3/part_1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+
+std::tuple<int, int> extract_pair(std::string str) {
+    int first, second;
+    int i = 4;
+    int start = i;
+    
+    while(str[i++] != ',') {}
+    first = std::stoi(std::string(str.begin() + start, str.begin() + i));
+    start = i;
+
+    while(str[i++] != ')') {}
+    second = std::stoi(std::string(str.begin() + start, str.begin() + i));
+
+    return std::make_tuple(first, second);
+}
+
+int main() {
+    std::ifstream file("../../day_3/in.input", std::ios::in | std::ios::binary | std::ios::ate);
+    long result = 0;
+    if (!file) {
+        std::cerr << "Couldn't open the file!" << '\n';
+        return 1;
+    }
+
+    std::streamsize size = file.tellg();
+    std::string buffer;
+    buffer.resize(size);
+    file.seekg(0, std::ios::beg);
+
+    if (!file.read(buffer.data(), size)) {
+        std::cerr << "Couldn't read file contents!" << '\n';
+        return 1;
+    }
+
+    std::regex pattern(R"(mul\([1-9][0-9]*,[1-9][0-9]*\))");
+
+    for (std::sregex_iterator it(buffer.begin(), buffer.end(), pattern), end_it; it != end_it; it++) {
+        std::smatch match = *it;
+        
+        std::tuple<int, int> nums = extract_pair(match[0].str());
+        result += std::get<0>(nums) * std::get<1>(nums);
+    }
+
+    std::cout << result << '\n';
+
+    return 0;
+}
